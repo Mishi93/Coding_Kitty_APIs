2396bfcd48ac5d57d32b2353ee12a57e99229c74
Mehwish Younus <131246728+Mishi93@users.noreply.github.com>
2026-08-03 10:13:05 +0500

    Update schemas.py

--- Backend/app/schemas.py
+++ Backend/app/schemas.py
@@ -4,7 +4,7 @@ from typing import List, Optional
 
 from pydantic import BaseModel, EmailStr, Field, field_validator
 
-from app.models import Gender
+from models import Gender
 
 # ---------------------------------------------------------------------
 # Auth Schemas
